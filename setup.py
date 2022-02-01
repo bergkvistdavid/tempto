918abc27410496e469e93ea15f36aa1ff4e78866
da6003be-s <da6003be-s@student.lu.se>
2022-02-01 20:18:10 +0100

    Licensing and setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="tempto",
+    version="0.0.1",
+    license="Apache License 2.0",
+    description="Retry",
+    author="bergkvistdavid",
+    packages=find_packages()
+)
